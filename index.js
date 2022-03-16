9a6b3aef13877fc393f24a0155d2b7ed5a90d544
Yamani Yuda <yamaniyuda@gmail.com>
2022-03-16 07:57:41 +0700

    membuat responsif

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const bannerWrapper = document.querySelector('.banner-wrapper')
 const banner = document.querySelector('.banner')
 const navbar = document.querySelector('.navbar-wrapper');
-  logo = document.querySelector('.navbar-logo')
+  logo = document.querySelector('.navbar-logo');
+  iconmenu = document.querySelector('.menee');
+  menu = document.querySelector('.navbar-menu-list')
+
+let mnCheck = false
 // let nav = false
 
 window.addEventListener('scroll',(e) => {
@@ -9,9 +13,22 @@ window.addEventListener('scroll',(e) => {
     navbar.style.background = 'white'
     navbar.style.borderBottom = '1px solid black'
     logo.style.color= 'black'
+    iconmenu.style.color = '#258541'
   } else {
     navbar.style.background = 'none'
     navbar.style.borderBottom = 'none'
     logo.style.color= 'white'
+    iconmenu.style.color = 'white'
+  }
+})
+
+iconmenu.addEventListener('click', e => {
+  if (!mnCheck) {
+    menu.style.display = 'block'
+    mnCheck = true
+  }
+  else {
+    menu.style.display = 'none'
+    mnCheck = false
   }
 })
